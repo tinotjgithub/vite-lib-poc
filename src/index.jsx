7084459ac6a5c20f26ac36ed76c98e7580e355bf
Tino Jose <tino.jose@tryg.dk>
2023-09-07 17:11:21 +0200

    saving changes

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,1 @@
-import React from "react";
-
-export function Card({ img, title, children}) {
-  return (
-    <article>
-      <img src={img.src} alt={img.alt} />
-      <h2>{title}</h2>
-      {children}
-    </article>
-  );
-}
+export * from "./Card";
